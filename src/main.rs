31f2eacf30cc249099e9fad9503b69fd634e4c4a
ethancarlsson <ethanmcarlsson@gmail.com>
2023-08-05 20:02:34 +0200

    Add measurements for sqlite with sync_off

--- src/main.rs
+++ src/main.rs
@@ -91,6 +91,43 @@ fn measure_logs_rdbms(iterations: usize) -> PerfResults {
     return results;
 }
 
+fn measure_logs_sqlite_sync_off(iterations: usize) -> PerfResults {
+    let conn = sqlite::Connection::open("sync_off").unwrap();
+    conn.execute(
+        "
+                  CREATE TABLE IF NOT EXISTS log_table (
+                        text    TEXT NOT NULL,
+                        time    INTEGER
+                      );
+                  DELETE FROM log_table;
+                  PRAGMA synchronous=OFF;
+                  ",
+    )
+    .unwrap();
+
+    let mut results = vec![];
+    for i in 0..iterations {
+        let now = SystemTime::now()
+            .duration_since(SystemTime::UNIX_EPOCH)
+            .unwrap();
+        let before = Instant::now();
+
+        conn.execute(format!(
+            "INSERT INTO log_table (text, time)  VALUES ('user made a request', {});",
+            now.as_secs()
+        ))
+        .unwrap();
+
+        results.push(before.elapsed());
+
+        if i % 10000 == 0 {
+            println!("{i}");
+        }
+    }
+
+    results
+}
+
 fn measure_logs_rdbms_no_id(iterations: usize) -> PerfResults {
     let mut client = Client::connect("postgresql://ethancarlsson@localhost/log_db", NoTls).unwrap();
 
@@ -129,11 +166,13 @@ fn generate_plot(perf_results: &PerfResults, plot_name: &str) {
 
     root_area.fill(&WHITE).unwrap();
 
+    let max_height: u128 = 500000;
+
     let mut ctx = ChartBuilder::on(&root_area)
-        .set_label_area_size(LabelAreaPosition::Left, 40)
-        .set_label_area_size(LabelAreaPosition::Bottom, 40)
+        .set_label_area_size(LabelAreaPosition::Left, 80)
+        .set_label_area_size(LabelAreaPosition::Bottom, 80)
         .caption(format!("{plot_name}"), ("sans-serif", 40))
-        .build_cartesian_2d(0..nanos.len(), 0..nanos.clone().max().unwrap_or(0))
+        .build_cartesian_2d(0..nanos.len(), 0..max_height)
         .unwrap();
 
     ctx.configure_mesh().draw().unwrap();
@@ -149,17 +188,16 @@ fn generate_comparison_plot(
     other: &PerfResults,
     other_name: &str,
 ) {
-    let name = format!("images/comparison.png");
+    let name = format!("images/rdbms_file_{other_name}.png");
     let root_area = BitMapBackend::new(&name, (1200, 800)).into_drawing_area();
 
     root_area.fill(&WHITE).unwrap();
 
     let nanos_rdbms = results_rdbms.iter().map(|duration| duration.as_nanos());
-    let nanos_other = other
-        .iter()
-        .map(|duration| duration.as_nanos());
+    let nanos_other = other.iter().map(|duration| duration.as_nanos());
     let nanos_file = results_file.iter().map(|duration| duration.as_nanos());
 
+    let max_height: u128 = 500000;
     let mut ctx = ChartBuilder::on(&root_area)
         .set_label_area_size(LabelAreaPosition::Left, 80)
         .set_label_area_size(LabelAreaPosition::Right, 20)
@@ -168,30 +206,42 @@ fn generate_comparison_plot(
             format!("rdbms vs file vs  rdbms {other_name} for logging"),
             ("sans-serif", 40),
         )
-        .build_cartesian_2d(
-            0..nanos_rdbms.len(),
-            0..nanos_rdbms.clone().max().unwrap_or(0),
-        )
+        .build_cartesian_2d(0..nanos_rdbms.len(), 0..max_height)
         .unwrap();
 
     ctx.configure_mesh().draw().unwrap();
 
-    let series_rdbms = LineSeries::new(nanos_rdbms.enumerate(), &GREEN);
-    let series_other = LineSeries::new(nanos_other.enumerate(), &BLUE);
-    let series_file = LineSeries::new(nanos_file.enumerate(), &RED);
+    let series_rdbms = LineSeries::new(nanos_rdbms.clone().enumerate(), &GREEN);
+    let series_other = LineSeries::new(nanos_other.clone().enumerate(), &BLUE);
+    let series_file = LineSeries::new(nanos_file.clone().enumerate(), &RED);
 
     ctx.draw_series(series_rdbms)
         .unwrap()
-        .label("rdbms")
+        .label(format!(
+            "rdbms avg {}ns",
+            nanos_rdbms.sum::<u128>() / u128::try_from(results_rdbms.len()).unwrap()
+        ))
         .legend(|(x, y)| PathElement::new(vec![(x, y), (x + 20, y)], &GREEN));
     ctx.draw_series(series_other)
         .unwrap()
-        .label(other_name)
+        .label(format!(
+            "{other_name} avg {}ns",
+            nanos_other.sum::<u128>() / u128::try_from(other.len()).unwrap()
+        ))
         .legend(|(x, y)| PathElement::new(vec![(x, y), (x + 20, y)], &BLUE));
     ctx.draw_series(series_file)
         .unwrap()
-        .label("files")
+        .label(format!(
+            "files avg {}ns",
+            nanos_file.sum::<u128>() / u128::try_from(results_file.len()).unwrap()
+        ))
         .legend(|(x, y)| PathElement::new(vec![(x, y), (x + 20, y)], &RED));
+
+    ctx.configure_series_labels()
+        .border_style(&BLACK)
+        .background_style(&WHITE.mix(0.8))
+        .draw()
+        .unwrap();
 }
 
 fn main() {
@@ -204,9 +254,22 @@ fn main() {
     let results_rdbms_no_id = measure_logs_rdbms_no_id(iterations);
     let results_rdbms = measure_logs_rdbms(iterations);
     let results_file = measure_logs_files(iterations);
+    let results_sqlite_sync_off = measure_logs_sqlite_sync_off(iterations);
 
     generate_plot(&results_rdbms, "rdbms");
     generate_plot(&results_file, "file");
 
-    generate_comparison_plot(&results_rdbms, &results_file, &results_rdbms_no_id, "results_rdbms_no_id")
+    generate_comparison_plot(
+        &results_rdbms,
+        &results_file,
+        &results_rdbms_no_id,
+        "results_rdbms_no_id",
+    );
+
+    generate_comparison_plot(
+        &results_rdbms,
+        &results_file,
+        &results_sqlite_sync_off,
+        "sqlite_sync_off",
+    )
 }
